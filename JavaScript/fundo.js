15cf4e7bc71cdc671f358ba47f0db94a9b4464bd
Italo Machado <italomachado8000@gmail.com>
2023-05-29 18:30:37 -0300

    Add files via upload

---
+++ JavaScript/fundo.js
@@ -0,0 +1,33 @@
+new FinisherHeader({
+    "count": 100,
+    "size": {
+      "min": 2,
+      "max": 8,
+      "pulse": 0
+    },
+    "speed": {
+      "x": {
+        "min": 0,
+        "max": 0.4
+      },
+      "y": {
+        "min": 0,
+        "max": 0.6
+      }
+    },
+    "colors": {
+      "background": "#201e30",
+      "particles": [
+        "#00b7ff"
+      ]
+    },
+    "blending": "screen",
+    "opacity": {
+      "center": 1,
+      "edge": 0
+    },
+    "skew": 0,
+    "shapes": [
+      "c"
+    ]
+  });
---
+++ JavaScript/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#texto', {
+    strings: ['FrontEnd ',' BackEnd.'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay:1000,
+    loop:true
+
+  });
